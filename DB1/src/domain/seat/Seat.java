d9e29447fe4c102960f3eb46768b8d0f7107f413
Jeong-Rae <kkwjdfo@gmail.com>
2024-05-08 16:58:26 +0900

    [Feat] 좌석 정보 엔티티 추가

---
+++ DB1/src/domain/seat/Seat.java
@@ -0,0 +1,37 @@
+package domain.seat;
+
+public class Seat {
+	private Long seatId;
+	private boolean isActive;
+	private Long cinemaId;
+
+	public Seat(long seatId, boolean isActive) {
+		this.seatId = seatId;
+		this.isActive = isActive;
+	}
+
+	public long getSeatId() {
+		return this.seatId;
+	}
+
+	public boolean getIsActive() {
+		return this.isActive;
+	}
+
+	public void updateIsActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public long getCinemaId() {
+		return this.cinemaId;
+	}
+
+	public void setCinemaId(Long cinemaId) {
+		this.cinemaId = cinemaId;
+	}
+
+	@Override
+	public String toString() {
+		return "Seat[" + "seatId=" + this.seatId + ", isActive=" + this.isActive + ", cinemaId=" + this.cinemaId + ']';
+	}
+}
