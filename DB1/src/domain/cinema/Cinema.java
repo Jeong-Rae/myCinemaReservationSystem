26a781044c73359d1798712142fed8fa954e9173
Jeong-Rae <kkwjdfo@gmail.com>
2024-05-08 16:35:30 +0900

    [feat] 극장 엔티티 추가

---
+++ DB1/src/domain/cinema/Cinema.java
@@ -0,0 +1,52 @@
+package domain.cinema;
+
+public class Cinema {
+	private Long cinemaId;
+	private boolean isActive;
+	private int seatRow;
+	private int seatCol;
+
+	public Cinema(boolean isActive, int seatRow, int seatCol) {
+		this.isActive = isActive;
+		this.seatRow = seatRow;
+		this.seatCol = seatCol;
+	}
+
+	public long getCinemaId() {
+		return this.cinemaId;
+	}
+
+	public void updateCinemaId(Long cinemaId) {
+		this.cinemaId = cinemaId;
+	}
+
+	public boolean getIsActive() {
+		return isActive;
+	}
+
+	public void updateIsActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public int getSeatRow() {
+		return this.seatRow;
+	}
+
+	public void updateSeatRow(int seatRow) {
+		this.seatRow = seatRow;
+	}
+
+	public int getSeatCol() {
+		return this.seatCol;
+	}
+
+	public void updateSeatCol(int seatCol) {
+		this.seatCol = seatCol;
+	}
+
+	@Override
+	public String toString() {
+		return "Cinema[" + "cinemaId=" + this.cinemaId + ", isActive=" + this.isActive + ", seatRow=" + this.seatRow
+				+ ", seatCol=" + this.seatCol + ']';
+	}
+}
