44bf808ec45713db620b5b871af804fb4a1ade68
Jeong-Rae <kkwjdfo@gmail.com>
2024-05-08 16:59:20 +0900

    [Feat] 티켓 정보 엔티티 추가

---
+++ DB1/src/domain/ticket/Ticket.java
@@ -0,0 +1,86 @@
+package domain.ticket;
+
+public class Ticket {
+	private Long ticketId;
+	private boolean isIssued;
+	private int standardPrice;
+	private int salePrice;
+	private Long screeningScheduleId;
+	private Long cinemaId;
+	private Long reservationId;
+	private Long seatId;
+
+	public Ticket(boolean isIssued, int standardPrice, int salePrice) {
+		this.isIssued = isIssued;
+		this.standardPrice = standardPrice;
+		this.salePrice = salePrice;
+	}
+
+	public Long getTicketId() {
+		return this.ticketId;
+	}
+
+	public boolean getIsIssued() {
+		return this.isIssued;
+	}
+
+	public void updateIsIssued(boolean isIssued) {
+		this.isIssued = isIssued;
+	}
+
+	public int getStandardPrice() {
+		return this.standardPrice;
+	}
+
+	public void updateStandardPrice(int standardPrice) {
+		this.standardPrice = standardPrice;
+	}
+
+	public int getSalePrice() {
+		return this.salePrice;
+	}
+
+	public void updateSalePrice(int salePrice) {
+		this.salePrice = salePrice;
+	}
+
+	public Long getScreeningScheduleId() {
+		return this.screeningScheduleId;
+	}
+
+	public void setScreeningScheduleId(Long screeningScheduleId) {
+		this.screeningScheduleId = screeningScheduleId;
+	}
+
+	public Long getCinemaId() {
+		return this.cinemaId;
+	}
+
+	public void setCinemaId(Long cinemaId) {
+		this.cinemaId = cinemaId;
+	}
+
+	public Long getReservationId() {
+		return this.reservationId;
+	}
+
+	public void setReservationId(Long reservationId) {
+		this.reservationId = reservationId;
+	}
+
+	public Long getSeatId() {
+		return seatId;
+	}
+
+	public void setSeatId(Long seatId) {
+		this.seatId = seatId;
+	}
+
+	@Override
+	public String toString() {
+		return "Ticket[" + "ticketId=" + this.ticketId + ", isIssued=" + this.isIssued + ", standardPrice="
+				+ this.standardPrice + ", salePrice=" + this.salePrice + ", screeningScheduleId="
+				+ this.screeningScheduleId + ", cinemaId=" + this.cinemaId + ", reservationId=" + this.reservationId
+				+ ", seatId=" + this.seatId + ']';
+	}
+}
