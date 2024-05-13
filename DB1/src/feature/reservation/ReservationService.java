6bbb4e685f48b3a33057e4e09715e9dcc246122e
Jeong-Rae <kkwjdfo@gmail.com>
2024-05-14 05:38:41 +0900

    [Feat] Update, Delete 코드 추가

--- DB1/src/feature/reservation/ReservationService.java
+++ DB1/src/feature/reservation/ReservationService.java
@@ -3,9 +3,12 @@ package feature.reservation;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import core.domain.reservation.PaymentStatusType;
 import core.domain.reservation.Reservation;
+import core.domain.ticket.TicketRequest;
 import infrastructure.config.DatabaseConfig;
 import infrastructure.repository.ReservationRepository;
 
@@ -21,6 +24,32 @@ public class ReservationService {
         }
 
         return response;
+    }
+    
+    // 예약 생성
+    public Reservation createReservation(ReservationRequest request) {
+    	int amout = 0;
+    	for (TicketRequest ticketRequest : request.ticketRequests()) {
+    		amout += ticketRequest.salePrice();
+    	}
+    	
+    	Reservation reservation = new Reservation(request.paymentMethodType(), PaymentStatusType.COMPLETED, amout, new Date());
+    	reservation.setMemberId(request.memberId());
+    	
+    	Long reservationId;
+    	
+    	try (Connection connection = DatabaseConfig.getConnectionUser()) {
+    		reservationId = reservationRepository.insertToReservation(connection, reservation);
+    		reservation.setReservationId(reservationId);
+    		
+    		System.out.println("[createReservation] reservation: "+ reservation);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    	
+    	
+    	return reservation;
+    	
     }
 
     public int updateReservation(String setClause) {
--- DB1/src/infrastructure/repository/ReservationRepository.java
+++ DB1/src/infrastructure/repository/ReservationRepository.java
@@ -1,6 +1,7 @@
 package infrastructure.repository;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -25,6 +26,34 @@ public class ReservationRepository {
         }
 
         return response;
+    }
+    
+    public Long insertToReservation(Connection connection, Reservation reservation) {
+    	String sql = "INSERT INTO reservation (payment_method, payment_status, amount, payment_date, member_id) VALUES (?, ?, ?, ?, ?)";
+    	Long reservationId = 0L;
+    	
+        try (PreparedStatement pstmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
+        	pstmt.setString(1, reservation.getPaymentMethod().name());
+            pstmt.setString(2, reservation.getPaymentStatus().name());
+            pstmt.setInt(3, reservation.getAmount());
+            pstmt.setDate(4, new java.sql.Date(reservation.getPaymentDate().getTime()));
+            pstmt.setLong(5, reservation.getMemberId());
+            
+            int affectedRows = pstmt.executeUpdate();
+            if (affectedRows > 0) {
+                try (ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        reservationId = generatedKeys.getLong(1);
+                    }
+                }
+            }
+            
+        } catch (SQLException e) {
+            System.out.println("[insertReservation] Reservation 테이블에 INSERT 실패");
+            e.printStackTrace();
+        }
+        
+        return reservationId;
     }
 
     public int updateReservationBySqlNative(Connection connection, String setClause) throws SQLException {
